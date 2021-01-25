8c0bbb741ee8c8def87cde40bfce690aff81d673
AnandT0 <40590302+AnandT0@users.noreply.github.com>
2021-01-25 23:31:29 +0530

    other updates

--- Code_SPNet/Code_for_mask.py
+++ Code_SPNet/Code_for_mask.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 
-file1 = open('gt_all_data.txt')
+# file1 = open('gt_all_data.txt')
+file1 = open('/content/Core_point_detection/Core_point_GT/Core_point_GT/FUT phase1/gt_all_data.txt')
 files = file1.readlines()
 for fil in files:
 	fil1 = fil.split(",")
-	img = cv2.imread('/media/biometric/Data21/Core_Point/data_used/' + fil1[0])
+	img = cv2.imread('/content/Core_point_detection/Core_point_GT/Core_point_GT/FUT phase1/data_used/' + fil1[0])
 	mask = np.zeros(img.shape[:2],dtype="uint8")
 	cv2.rectangle(mask,(int(fil1[1]),int(fil1[2])),(int(fil1[3]),int(fil1[4])),255,-1)
-	output = '/media/biometric/Data21/Core_Point/Mask_gt/' + (fil1[0])
+	output = '/content/Core_point_detection/Core_point_GT/Core_point_GT/FUT phase1/Mask_gt/' + (fil1[0])
 	cv2.imwrite(output,mask)	
 
--- Code_SPNet/Grountruth_in_one_file.py
+++ Code_SPNet/Grountruth_in_one_file.py
@@ -1,11 +1,12 @@
 import os
 import os.path
-path1 = '/media/biometric/Data21/Core_Point/gTruth'    #path of folder of textfile
+# path1 = '/media/biometric/Data21/Core_Point/gTruth'    #path of folder of textfile
+path1 = '/content/Core_point_detection/Core_point_GT/Core_point_GT/FUT phase1/gTruth'
 List_txt =  os.listdir(path1)
-fw = open('gt_all_data.txt' , 'w+')
+fw = open('/content/Core_point_detection/Core_point_GT/Core_point_GT/FUT phase1/gt_all_data.txt' , 'w+')
 class1 = '0'
 for file1 in List_txt:
-	fo = open(path1 + '/' + file1 , "rw+")
+	fo = open(path1 + '/' + file1 , "r+")
 	p = (file1.split('.')[0])[:-3] + ".bmp"
 	print(p)
 	line = fo.readlines()
